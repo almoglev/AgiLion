8d06b8ae2fe27119c9a19cd6633eff67d1817cf4
Almog Lev <almoglev3@gmail.com>
2022-01-28 14:04:37 +0200

    Error and Exceptions handling

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require('express-async-handler')
+
+
+// @desc    Register a new user
+// @route   /api/users
+// @access  Publish
+const registerUser = asyncHnadler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    // Validation
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+
+    res.send('Register Route')
+})
+
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Publish
+const loginUser = asyncHnadler(async (req, res) => {
+    res.send('Login Route')
+})
+
+module.exports = {
+    registerUser,
+    loginUser
+}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
-// import packeges
+// Imports
 const express =  require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 5000
 const app = express()
-// create route with express
+
+// Allow sending raw json
+app.use(express.json())
+
+// Accept the url encoded form
+app.use(express.urlencoded({extended: false}))
+
+// Create route with express
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Welcome to Agilion API'})
 })
-app.listen(PORT, ()=>console.log(`Listening on port ${PORT}...`))
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, ()=>console.log(`Server is listening on port ${PORT}`))
 
