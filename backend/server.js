74826b0482f88b0d2ef8b521c565512579aa3f36
Almog Lev <almoglev3@gmail.com>
2022-01-28 12:16:40 +0200

    Set up a basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Server...')
+// import packeges
+const express =  require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+const app = express()
+// create route with express
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to Agilion API'})
+})
+app.listen(PORT, ()=>console.log(`Listening on port ${PORT}...`))
+
