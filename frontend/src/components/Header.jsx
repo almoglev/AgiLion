5d4a069c64d26cbc4be1ca2c32f2c66cf0cd0df1
Almog Lev <almoglev3@gmail.com>
2022-01-28 17:33:19 +0200

    Home, Login and Register UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { GiLion } from "react-icons/gi";
 import {Link} from 'react-router-dom'
 
 function Header() {
   return( 
     <header className='header'>
         <div className='logo'>
-            <Link to='/'>Agilion</Link>
+            <Link to='/'><GiLion style={{transform: 'scale(1.8)'}} />&nbsp;&nbsp;&nbsp;Agilion</Link>
         </div>
         <ul>
             <li>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {FaMarker, FaBoxOpen} from 'react-icons/fa'
 
 function Home() {
-  return <div><h1>Home</h1></div>;
+    return( 
+        <>
+            <section className='heading'>
+                <h1>What do you need help with?</h1>
+                <p>Please choose an option below</p>
+            </section>
+
+            <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+                <FaMarker />Write a new ticket
+            </Link>
+
+            <Link to='/tickets' className='btn btn-block'>
+                <FaBoxOpen />All my tickets
+            </Link>
+        </>
+    )
 }
 
 export default Home;
