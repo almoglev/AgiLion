6d2f341e281cce4937a7d37b17bf79a0df34c95d
Almog Lev <almoglev3@gmail.com>
2022-01-31 15:07:37 +0200

    Create tickets functionality

--- frontend/src/features/tickets/ticketsSlice.js
+++ frontend/src/features/tickets/ticketsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { set } from 'mongoose'
 import ticketService from './ticketsService'
 
 const initialState = {
@@ -10,6 +11,19 @@ const initialState = {
     message: ''
 }
 
+// Create a new ticket
+export const createTicket = createAsyncThunk('tickets/create', async (ticketData, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await ticketService.createTicket(ticketData, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message)
+                        || error.message || error.toString()
+
+                        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 export const ticketSlice = createSlice({
     name: 'ticket',
     initialState,
@@ -17,6 +31,19 @@ export const ticketSlice = createSlice({
         reset: (state) => initialState
     },
     extraReducers: (builder) => {
+        builder
+        .addCase(createTicket.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(createTicket.fulfilled, (state) => {
+            state.isLoading = false
+            state.isSuccess = true
+        })
+        .addCase(createTicket.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })
 
     }
 })
