50707c6c42bd4c30a2effeab4ec95a88a1b8e067
Almog Lev <almoglev3@gmail.com>
2022-01-31 20:06:19 +0200

    BE  Notes functionality -model, controller, routes

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const {protect} = require('../middleware/authMiddleware')
 const {getTickets, getTicketById, createTicket, deleteTicketById, updateTicketById} = require('../controllers/ticketController')
 
+// Re-route into note router 
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 // The requests (go to ticketController.js)
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 router.route('/:id').get(protect, getTicketById).delete(protect, deleteTicketById).put(protect, updateTicketById)
